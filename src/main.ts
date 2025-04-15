50bf0e1c076da32f38a3b433cc9f30b7814d9af5
Virgo <2811132560@qq.com>
2025-04-15 22:58:15 +0800

    ci: 引入scss作为css的预解析器

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
+import '@/styles/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import router from './router'
 
